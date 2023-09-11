fc7bf41721c9269a5019b001b7a18c50206eaded
Ahmed Ahmedov <Murphy-mos@mail.ru>
2023-09-11 23:41:24 +0300

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Ahmed Ahmedov on 11.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
